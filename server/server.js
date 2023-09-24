49d414ded114c84e15255efa1d5bfd4f4efa0eaf
Siva Raja Surya E <sivarajasurya16@gmail.com>
2023-09-24 20:22:22 +0530

    node 16+

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-const path = require("path");
-const http = require("http");
-const express = require("express");
-const socketIO = require("socket.io");
+import { join } from "path";
+import { createServer } from "http";
+// import express, { static } from "express";
+import express from "express";
+import socketIO from "socket.io";
 
-const { generateMessage , generateLocationMessage} = require("./utils/message");
-const publicPath = path.join(__dirname, "/../public");
+import { generateMessage, generateLocationMessage } from "./utils/message";
+const publicPath = join(__dirname, "/../public");
 const port = process.env.PORT || 3000;
 let app = express();
-let server = http.createServer(app);
+let server = createServer(app);
 let io = socketIO(server);
 
 app.use(express.static(publicPath));
